4f1e03824982866867eb0760464e7b581d8f0d36
eli hasson <elihasson@gmail.com>
2022-08-23 18:07:44 +0300

    test3

--- cmps/book-header.jsx
+++ cmps/book-header.jsx
@@ -6,7 +6,7 @@ function _AppHeader(props) {
 				Welcome To
 				<span className="logo">
 					<div className="image-container">
-						<img src="./assets/img/CT-logos_white.png" alt="hi chaim" />
+						<img src="./assets/img/CT-logos_white.png" alt="poki" />
 					</div>
 				</span>
 				Book Shop!
